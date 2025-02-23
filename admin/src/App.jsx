05b0c359f2b700e282a480375e5d09fbe3e46a2f
Arifata <arifetamulugeta@gmail.com>
2025-02-24 01:23:57 +0300

    Admin ui set-up is created

---
+++ admin/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import SideBar from './components/sidebar/SideBar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <hr/>
+      <div className="app-content">
+        <SideBar/>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Sidebar.css'
+const SideBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SideBar
