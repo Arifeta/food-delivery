3cdacd2672c4500e00f241b7752d5e055f9c81dd
Arifata <arifetamulugeta@gmail.com>
2025-02-28 18:18:02 +0300

    add item functionality added and integrated with backend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import SideBar from './components/sidebar/SideBar'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import SideBar from './components/sidebar/SideBar';
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Order from './pages/Order/Order';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <hr/>
+      <Navbar />
+      <hr />
       <div className="app-content">
-        <SideBar/>
+        <SideBar />
+        <Routes>
+          <Route path="/add" element={<Add />} />   {/* ✅ Correct */}
+          <Route path="/list" element={<List />} /> {/* ✅ Correct */}
+          <Route path="/orders" element={<Order />} /> {/* ✅ Correct */}
+        </Routes>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
