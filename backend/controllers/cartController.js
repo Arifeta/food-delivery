4baab5c7eda6c29fad832644c29bb80de166e489
Arifata <arifetamulugeta@gmail.com>
2025-03-02 07:33:12 +0300

    remove from cart api completed

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -28,7 +28,24 @@ const addToCart = async (req, res) => {
 
 // remove items from user cart
 const removeFromCart = async (req, res) => {
-   
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if (cartData[req.body.itemId]>0) {
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData})
+        res.json({
+            success: true,
+            message: "Item removed from cart"
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: "Error"
+        }) 
+    }
 }
 
 // fetch user cart data
