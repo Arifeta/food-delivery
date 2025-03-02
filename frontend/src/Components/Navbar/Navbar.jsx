de54d08b4b479f7348eb31d033cf38d9c757665b
Arifata <arifetamulugeta@gmail.com>
2025-03-02 04:54:00 +0300

    login and sign up is updated

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -9,16 +9,16 @@ const Navbar = ({setShowLogin}) => {
 
   const [menu, setMenu] = useState("menu");
 
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const {getTotalCartAmount, token, setToken} = useContext(StoreContext);
 
   return (
     <div className="navbar">
       <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
-        <Link to={'/'}  onClick={() => setMenu("home")}   className={menu === "home" ? "active" : ""}> home </Link>
-        <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}> menu </a>
-        <a href="#app-download" onClick={() => setMenu("mobile-app")}  className={menu === "mobile-app" ? "active" : ""}>mobile-app </a>
-        <a href="#footer" onClick={() => setMenu("contact-us")}className={menu === "contact-us" ? "active" : ""}> contact us</a>
+        <Link to={'/'}  onClick={() => setMenu("home")}   className={menu === "home" ? "active" : ""}> Home </Link>
+        <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}> Menu </a>
+        <a href="#app-download" onClick={() => setMenu("mobile-app")}  className={menu === "mobile-app" ? "active" : ""}>Mobile-app </a>
+        <a href="#footer" onClick={() => setMenu("contact-us")}className={menu === "contact-us" ? "active" : ""}> Contact Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -26,7 +26,16 @@ const Navbar = ({setShowLogin}) => {
            <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)} >sign in</button>
+        {!token?<button onClick={()=>setShowLogin(true)} >sign in</button>
+        :<div className="navbar-profile">
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropddown">
+              <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+              <hr />
+              <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+            </ul>
+          </div>}
+        
       </div>
     </div>
   );
