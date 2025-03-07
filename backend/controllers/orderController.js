076a9bad30c1ed8d0aa8a33934182de64342f865
Arifata <arifetamulugeta@gmail.com>
2025-03-08 02:07:31 +0300

    user orders display api is added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -42,12 +42,12 @@ const placeOrder = async (req, res) => {
         })
 
         const session = await stripe.checkout.sessions.create({
-            line_items: line_items,
+            line_items:line_items,
             mode: 'payment',
             success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`, // Should be cancel_url, not success_url
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`, 
         });
-        
+         
         res.json({
             success:true,
             session_url:session.url
@@ -87,5 +87,22 @@ const verifyOrder = async (req,res) => {
         })
     }
 }
+// user orders for frontend
+const userOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId})
+        res.json({
+            success: true,
+            data: orders
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message:"Error"
+        })
+    }
+}
 
-export {placeOrder, verifyOrder}
+
+export {placeOrder, verifyOrder, userOrders}
